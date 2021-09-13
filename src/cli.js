78a6840632e1ec10c9b526a5693fd75a509f213f
williamlark <william.lark@boldium.com>
2021-09-13 16:45:00 -0700

    feat: add progress spinner

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,18 @@
 import { runCarbonTest, prettyPrintResults } from "./website-carbon.js";
+import ora from 'ora';
 
 export function cli(args) {
   args = args.slice(2);
   let url = args[0] || undefined;
   if(url) {
-    console.log(`🚀 Running carbon test for ${url}...`)
+    const spinner = ora(`Running carbon test for ${url}...`).start();
     runCarbonTest(url)
       .then(json => {
+        spinner.succeed()
         prettyPrintResults(json)
       })
       .catch(e => {
+        spinner.fail()
         console.log(`🚩 ERROR: `, e)
       })
   }
